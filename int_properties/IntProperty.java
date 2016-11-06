c3123051aa15d84d89a3efedb93515d87805a940
James Morrow <jamesmorrowdesign@gmail.com>
2016-11-06 02:18:20 -0500

    Deleted errant folder.

--- int_properties/IntProperty.java
+++
@@ -1,6 +0,0 @@
-package int_properties;
-
-public interface IntProperty {
-	public int evaluate(long x);
-	public int numPossibleStates();
-}
--- int_properties/IntSequence.java
+++
@@ -1,7 +0,0 @@
-package int_properties;
-
-import java.util.function.IntPredicate;
-
-public interface IntSequence extends IntProperty {
-	public long nth(int n);
-}
